3a571ccf58221e0a6eb70f769b0d0cf8ef4de0b1
sujatakuma <sujata030420@gmail.com>
2023-05-21 20:53:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import IndianNews from './IndianNews'
+
+const App = () => {
+  return (
+    <div>
+      <IndianNews></IndianNews>
+    </div>
+  )
+}
+
+export default App
